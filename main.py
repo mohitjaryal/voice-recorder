3690ffc7242c40140dbac1b22387ce5da58bae43
Mohit <mohitt.tech@gmail.com>
2026-03-08 12:26:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ freq = 44100
 # recording duration
 record_duration = 5
 
+# start recoder 
+recording = sd.rec(int(record_duration * freq),
+                        samplerate=freq, channels=2)
+
+# record audio for given. no of seconds
+
